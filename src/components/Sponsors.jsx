e04678609c80d49c93659c3520567c7ea3847972
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2025-12-25 00:51:33 +0500

    Refactor code structure for improved readability and maintainability

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -9,7 +9,7 @@ const sponsorLogos = [
   "/logos/csit.jpg",
   "/logos/hec_sindh.jpeg",
   "https://www.iccs.edu/esims/images/iccbs_logo.png",
-  "https://scontent.fkhi2-2.fna.fbcdn.net/v/t39.30808-6/308203578_403708711956831_763658589569409051_n.png?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=-RuNaSqQpmAQ7kNvwF4Pa77&_nc_oc=Adke4LIHIh0BE7Qi0NXLFK3nqeC6M9yeviC4RCLYbNqXpd001DP_4azIdcgja4BR0Qw&_nc_zt=23&_nc_ht=scontent.fkhi2-2.fna&_nc_gid=-5iB7sTssEsCrmFF0jK5RA&oh=00_AfkOCUiSsD5l5sGEiJOl9MmwrF2gWwTZQVsHVkv7wZ1SFw&oe=69423630",
+  "/logos/ieeecomp.png",
   "https://api.ieeensusb.org/media_files/main_website_files/About/IEEE/About_Image/e4c81f5dd1d43164425630c4ab971bd5.jpeg",
   "https://macs.iobm.edu.pk/wp-content/uploads/2022/06/IEEE-karachi.png",
 ];
